c8a4fabf7eeedb8b4b4e98b446a6910d83346fd3
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-04-24 11:59:50 -0400

    Add files via upload
     Este programa solicita al usuario una cantidad determinada de números e identifica cuántos son positivos, negativos, pares e impares. 
    Luego de ingresar cada número, muestra un resumen acumulado de esas categorías.

---
+++ ejer.21 bucles/ejer.21 bucles/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ejer._21_bucles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidadNumeros;
+            Console.WriteLine("¿Cuántos números va a ingresar?");
+            cantidadNumeros = int.Parse(Console.ReadLine());
+
+            int positivos = 0, negativos = 0, pares = 0, impares = 0;
+
+            for (int i = 0; i < cantidadNumeros; i++)
+            {
+                {
+                    Console.WriteLine($"Ingrese el número {i + 1}:");
+                    int numero = Convert.ToInt32(Console.ReadLine());
+
+                    if (numero > 0) positivos++;
+                    else if (numero < 0) negativos++;
+
+                    if (numero % 2 == 0) pares++;
+                    else impares++;
+
+                }
+                Console.WriteLine($"Positivos: {positivos}, Negativos: {negativos}, Pares: {pares}, Impares: {impares}");
+
+            }
+        
+
+
+
+
+
+
+
+    }
+    }
+}
