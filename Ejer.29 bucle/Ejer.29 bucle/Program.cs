14d14133e268e2e7457a87c0bde892a38c75bc41
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-04-24 21:41:24 -0400

    Add files via upload
    Este programa genera un triángulo de asteriscos según el número de filas ingresado.

---
+++ Ejer.29 bucle/Ejer.29 bucle/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ejer._29_bucle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el número de filas para el triángulo: ");
+            int filas = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= filas; i++) 
+            {
+                for (int j = 1; j <= i; j++) 
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(); 
+
+            }
+        }
+    }
+}
+
