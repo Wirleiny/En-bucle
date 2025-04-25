440c7413f01a9bf8b04cb9cef45d841ac84b6fbd
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-04-24 21:28:41 -0400

    Add files via upload
    Muestra los números impares del 1 al 30 usando un bucle for.

---
+++ ejer.26 bucle/ejer.26 bucle/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ejer._26_bucle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A continuación se muestran los números impares del 1 al 30:");
+            for (int i = 1; i <= 30; i++) 
+            {
+                if (i % 2 != 0)
+                {
+
+                    Console.WriteLine(i); 
+                }
+            }
+        }
+    }
+}
+    
+
