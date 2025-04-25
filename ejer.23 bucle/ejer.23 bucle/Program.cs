c832a1c1f613767b3b27686e12401124e3386637
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-04-24 21:16:37 -0400

    Add files via upload
    Este programa solicita al usuario una cantidad de números, los va sumando
    y al final determina si la suma total es mayor o igual a 100, o si es menor.

---
+++ ejer.23 bucle/ejer.23 bucle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ejer._23_bucle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Cuántos números va a ingresar?");
+            int cantidadNumeros = Convert.ToInt32(Console.ReadLine());
+
+            int suma = 0; 
+
+            for (int i = 0; i < cantidadNumeros; i++)
+
+            {
+                Console.WriteLine($"Ingrese el número {i + 1}:");
+                int numero = Convert.ToInt32(Console.ReadLine());
+                suma += numero; 
+            }
+
+            
+            if (suma >= 100)
+            {
+                Console.WriteLine("La sumatoria es mayor o igual a 100.");
+            }
+            else
+            {
+                Console.WriteLine("La sumatoria es menor a 100.");
+            }
+        }
+    }
+}
+    
