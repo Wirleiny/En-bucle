e612ae5db5c84d09ccf0332a77ec662ce4bbd229
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-04-24 21:35:41 -0400

    Add files via upload
    Suma números ingresados por el usuario hasta que este introduzca un 0.

---
+++ ejer.28 bucle/ejer.28 bucle/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ejer._28_bucle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0; 
+            int numero; 
+
+            do
+            {
+                Console.WriteLine("Ingrese un número (0 para terminar):");
+                numero = Convert.ToInt32(Console.ReadLine()); 
+
+                suma += numero; 
+
+            } while (numero != 0); 
+
+           
+            Console.WriteLine($"La sumatoria de los números ingresados es: {suma}");
+        }
+    }
+}
+    
+
