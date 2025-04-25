6a7525511cb510e83b059b33ee9aca271f54b1ee
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-04-24 21:18:00 -0400

    Add files via upload
    Este programa suma los primeros 100 números naturales (del 1 al 100)
     y muestra el resultado por consola.

---
+++ ejer.24 bucle/ejer.24 bucle/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ejer._24_bucle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0; 
+
+            
+            for (int i = 1; i <= 100; i++)
+            {
+                suma += i; 
+            }
+
+            Console.WriteLine($"La suma de los primeros 100 números naturales es: {suma}");
+        }
+    }
+}
+    
