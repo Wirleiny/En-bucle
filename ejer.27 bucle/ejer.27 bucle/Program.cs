7ec133aaf0edda62637c223743a48635c72594c1
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-04-24 21:31:38 -0400

    Add files via upload
    Solicita un número y muestra su tabla de multiplicar del 1 al 12.

---
+++ ejer.27 bucle/ejer.27 bucle/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ejer._27_bucle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número para mostrar su tabla de multiplicar:");
+            int numero = Convert.ToInt32(Console.ReadLine()); 
+
+            for (int i = 1; i <= 12; i++) 
+            {
+                int resultado = numero * i; 
+                Console.WriteLine($"{numero} x {i} = {resultado}"); 
+            }
+        }
+    }
+}
+    }
+}
